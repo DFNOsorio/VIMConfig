48aac14080a32b08339fc3c2393f959c5bb0753b
DanielOsorio <vdosavh@gmail.com>
2018-04-30 13:46:28 +0100

    install.sh changed because colors

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-ln -s ~/.vim/vimrc ~/.vimrc
+:ln -s ~/.vim/vimrc ~/.vimrc
 cd ~/.vim
 git submodule init
 git submodule init
 git submodule foreach git pull origin master
 
+cd ~/.vim/bundle/colorschemes
+cp colors/* ~/.vim/colors
+
 cd ./bundle/youcompleteme
 git submodule update --init --recursive
 sudo python3 ./install.py
-
-
