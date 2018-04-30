36788805f093b506052e3470f264e7de09d093f7
DanielOsorio <vdosavh@gmail.com>
2018-04-30 13:39:38 +0100

    Install youcompleteme.vim bundle as a submodule.

---
+++ install.sh
@@ -0,0 +1,11 @@
+ln -s ~/.vim/vimrc ~/.vimrc
+cd ~/.vim
+git submodule init
+git submodule init
+git submodule foreach git pull origin master
+
+cd ./bundle/youcompleteme
+git submodule update --init --recursive
+sudo python3 ./install.py
+
+
