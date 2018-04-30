5c96383ae7d490d664f832f10b403a952207f624
Daniel Osório <vdosavh@gmail.com>
2018-04-30 16:55:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ cd ~/.vim
 git submodule init
 git submodule foreach git pull origin master
 
-cd ./bundle/youcompleteme
+cd ~/.vim/bundle/youcompleteme
 git submodule update --init --recursive
 sudo python3 ./install.py
+
+cd ~/.vim/bundle/tagbar
+git submodule add https://github.com/universal-ctags/ctags.git ctags
+cd ctags
+./autogen.sh
+./configure --prefix=/where/you/want # defaults to /usr/local
+make
+make install # may require extra privileges depending on where to install
